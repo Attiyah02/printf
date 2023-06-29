3d7e165a747388b540cdb492dc926a91e8d61b0a
ZiphoZZ <nkunkuzazz@gmail.com>
2023-06-29 06:17:03 -0700

    binary number conversion fuction

--- print_binary.c
+++ print_binary.c
@@ -1,38 +1,48 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-void print_binary(unsigned int num) {
-	    if (num > 1) {
-		            print_binary(num / 2);
-			        }
-	        printf("%d", num % 2);
+/*
+ * print_binary - print binary numbers
+ * _custom_printf - printf function to help print binary numbers
+ * @num: number to be converted to binaru number
+ */
+
+void print_binary(unsigned int num)
+{
+	if (num > 1)
+	{
+		print_binary(num / 2);
+	}
+	printf("%d", num % 2);
 }
 
-void custom_printf(const char* format, ...) {
-	    va_list args;
-	        va_start(args, format);
+void _custom_printf(const char *format, ...)
+{
+	va_list args;
 
-		    while (*format) {
-			            if (*format == '%') {
-					                format++;
-							            if (*format == 'b') {
-									                    unsigned int num = va_arg(args, unsigned int);
-											                    print_binary(num);
-													                } else {
-																                putchar(*format);
-																		            }
-								            } else {
-										                putchar(*format);
-												        }
-				            format++;
-					        }
+	va_start(args, format);
 
-		        va_end(args);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == 'b')
+			{
+				unsigned int num = va_arg(args, unsigned int);
+
+				print_binary(num);
+			}
+			else
+			{
+				putchar(*format);
+			}
+		}
+		else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
 }
-
-int main() {
-	    unsigned int num = 42;
-	        custom_printf("Binary representation of %u is %b\n", num, num);
-		    return 0;
-}
-
