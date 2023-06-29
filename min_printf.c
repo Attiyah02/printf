043566dc8209697da0286aa1bd067936d1429c60
ZiphoZZ <nkunkuzazz@gmail.com>
2023-06-29 02:33:00 -0700

    First code fix

--- min_printf.c
+++ min_printf.c
@@ -2,14 +2,19 @@
 #include <string.h>
 
 /*
- *
- *
+ * _printf - function that handles c, s, and literal %
+ * @ch: variable that stores characters.
+ * @str: varibale that store strings
+ * @p: pointer to the next format
+ * @ap: argument pointer
  */
+
 int _printf(const char *format, ...)
 {
 	va_list ap;
 	char *p, *str;
 	int ch;
+
 	va_start(ap, format);
 
 	for (p = format; *p; p++)
@@ -19,21 +24,21 @@ int _printf(const char *format, ...)
 			putchar(*p);
 			continue;
 		}
-		switch(*p++)
+		switch (*p++)
 		{
-			case 'c':
-			       	ch = va_arg(ap, int);
+			case'c':
+				ch = va_arg(ap, int);
 				putchar(ch);
 				*p++;
 				break;
 
-			case 's':
+			case's':
 				str = va_arg(ap, char *);
 				fputs(str, stdout);
 				*p += strlen(str);
 				break;
 
-			case '%':
+			case'%':
 				putchar('%');
 				*p++;
 				break;
